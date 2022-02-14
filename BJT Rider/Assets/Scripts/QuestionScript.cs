243f9703c2642cb917f2f7f9d8be8d2074e5c01f
Aklovya Gupta <ak2305.2001@gmail.com>
2022-02-14 13:41:42 +0530

    added lives script

---
+++ BJT Rider/Assets/Scripts/QuestionScript.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionScript : MonoBehaviour
+{
+
+    public int correctAnswer = 1;
+    public GameObject doorsLeft;
+    public GameObject doorsRight;
+    public GameObject questionCanvas;
+    public LifeScript lifeScript;
+    public void OnFirstOption(){
+        if(correctAnswer == 1){
+            doorsLeft.transform.Rotate(0,-90,0);
+            doorsRight.transform.Rotate(0,-90,0);
+            doorsLeft.GetComponent<BoxCollider>().enabled = false;
+            doorsRight.GetComponent<BoxCollider>().enabled = false;
+            questionCanvas.SetActive(false);
+        }else{
+
+            Debug.Log("WrongAnswer");
+            lifeScript.jaan = lifeScript.jaan - 1;
+            if(lifeScript.jaan == 0){
+                Debug.Log("Game Over");
+            }
+
+        }
+    }
+
+    public void OnSecondOption(){
+        if(correctAnswer == 2){
+            doorsLeft.transform.Rotate(0,-90,0);
+            doorsRight.transform.Rotate(0,-90,0);
+            doorsLeft.GetComponent<BoxCollider>().enabled = false;
+            doorsRight.GetComponent<BoxCollider>().enabled = false;
+            questionCanvas.SetActive(false);
+        }else{
+
+            Debug.Log("WrongAnswer");
+            lifeScript.jaan = lifeScript.jaan - 1;
+            if(lifeScript.jaan == 0){
+                Debug.Log("Game Over");
+            }
+        }
+    }
+
+    public void OnThirdOption(){
+        if(correctAnswer == 3){
+            doorsLeft.transform.Rotate(0,-90,0);
+            doorsRight.transform.Rotate(0,-90,0);
+            doorsLeft.GetComponent<BoxCollider>().enabled = false;
+            doorsRight.GetComponent<BoxCollider>().enabled = false;
+            questionCanvas.SetActive(false);
+        }else{
+
+            Debug.Log("WrongAnswer");
+            lifeScript.jaan = lifeScript.jaan - 1;
+            if(lifeScript.jaan == 0){
+                Debug.Log("Game Over");
+            }
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
